d7704757214988dc800861dc5d4a6f90db416407
= <=>
2024-01-18 11:48:22 -0300

    Add project files.

---
+++ WebApi/Controllers/AddCustomerController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApi.Models.AddCustomer;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddCustomerController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult AddCustomer(AddCustomerInput input)
+        {
+            return Created("", input);
+        }
+    }
+
+    
+}
---
+++ WebApi/Models/AddCustomer/AddCustomerInput.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApi.Models.AddCustomer
+{
+    public class AddCustomerInput
+    {
+        public string name { get; set; } = String.Empty;
+        public string email { get; set; } = String.Empty;
+        public string document { get; set; } = String.Empty;
+    }
+}
