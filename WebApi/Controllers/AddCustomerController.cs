7353a42004f1c182bb94c81f9be676b4fdd0e7c5
= <=>
2024-01-18 12:14:56 -0300

    Implementação do método AddCustomer

--- WebApi/Controllers/AddCustomerController.cs
+++ WebApi/Controllers/AddCustomerController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Domain.Contracts.UseCase.AddCustomer;
+using Microsoft.AspNetCore.Mvc;
 using WebApi.Models.AddCustomer;
 
 namespace WebApi.Controllers
@@ -7,12 +8,21 @@ namespace WebApi.Controllers
     [ApiController]
     public class AddCustomerController : ControllerBase
     {
+        private readonly IAddCustomerUseCase _addCustomerUseCase;
+
+        public AddCustomerController(IAddCustomerUseCase addCustomerUseCase)
+        {
+            this._addCustomerUseCase = addCustomerUseCase;
+        }
+
         [HttpPost]
         public IActionResult AddCustomer(AddCustomerInput input)
         {
+            var customer = new Domain.Entities.Customer(input.name, input.email, input.document);
+            _addCustomerUseCase.AddCustomer(customer);
             return Created("", input);
         }
     }
 
-    
+
 }
